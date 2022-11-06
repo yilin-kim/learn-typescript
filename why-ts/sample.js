c90cdb9e3328f18ca90684aeaac429404321091e
yilin-kim <irenek91@korea.ac.kr>
2022-11-07 02:39:49 +0900

    자바스크립트에서 @ts-check 주석 달기

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,7 +1,21 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// // sum(10, "20"); // 1020
+// let res = sum(10, 20); // 30
+// res.tolocaleString(); // ts가 아니기 때문에 자동으로 메소드 추천이 안됨
+
+//JS에서 나름 TS같은 기능을 사용할 수 있는 방법
+//@ts-check
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns
+ */
 function sum(a, b) {
   return a + b;
 }
 
-// sum(10, "20"); // 1020
-let res = sum(10, 20); // 30
-res.tolocaleString(); // ts가 아니기 때문에 자동으로 메소드 추천이 안됨
+// sum(10, "20");
