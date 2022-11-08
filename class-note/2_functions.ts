9efefc1494f971aa9c07d7a666cc9f1563bdaf3c
yilin-kim <irenek91@korea.ac.kr>
2022-11-08 16:43:06 +0900

    타입스크립트 함수 타입 정의 마무리

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -13,3 +13,9 @@ function add(): number {
 function sum(a: number, b: number): number {
   return a + b;
 }
+sum(10, 20); // 30
+// sum(10, 20, 30, 40); // TS에서는 엄격하게 스펙 체크
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {}
+log("hello ts");
