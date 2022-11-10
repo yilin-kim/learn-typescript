cede3b48329c5f7753bc219535392e065ce8961f
yilin-kim <irenek91@korea.ac.kr>
2022-11-10 23:26:32 +0900

    todo 실습 프로젝트 interface 적용

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,11 +1,16 @@
-let todoItems: { id: number, title: string, done: boolean}[];
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems(): { id: number, title: string, done: boolean}[] {
+function fetchTodoItems(): Todo[] {
   const todos = [
-    { id: 1, title: '안녕', done: false },
-    { id: 2, title: '타입', done: false },
-    { id: 3, title: '스크립트', done: false },
+    { id: 1, title: "안녕", done: false },
+    { id: 2, title: "타입", done: false },
+    { id: 3, title: "스크립트", done: false },
   ];
   return todos;
 }
@@ -16,7 +21,7 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: { id: number, title: string, done: boolean}): void {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
@@ -24,7 +29,7 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index:number, todo: { id: number, title: string, done: boolean}): void {
+function completeTodo(index: number, todo: Todo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -35,7 +40,7 @@ function logFirstTodo() {
 }
 
 function showCompleted() {
-  return todoItems.filter(item => item.done);
+  return todoItems.filter((item) => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
@@ -44,7 +49,7 @@ function addTwoTodoItems(): void {
   for (let i = 0; i < 2; i++) {
     addTodo({
       id: i,
-      title: '새 할 일',
+      title: "새 할 일",
       done: false,
     });
   }
